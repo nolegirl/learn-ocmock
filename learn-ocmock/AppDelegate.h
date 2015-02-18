a44b5aad86fa607020de0fa172e892116670f823
Mechelle Sieglitz <mechelle.sieglitz@detroitlabs.com>
2015-02-18 12:05:48 -0500

    First Commit

---
+++ learn-ocmock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  learn-ocmock
+//
+//  Created by Mechelle Sieglitz on 2/18/15.
+//  Copyright (c) 2015 Mechelle Sieglitz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ learn-ocmock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  learn-ocmock
+//
+//  Created by Mechelle Sieglitz on 2/18/15.
+//  Copyright (c) 2015 Mechelle Sieglitz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
